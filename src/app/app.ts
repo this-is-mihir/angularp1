43c94f7c42254a7b18a9ae51c819b9790c2eaedc
this-is-mihir <patelmihir0367@gmail.com>
2026-01-12 17:22:50 +0530

    p1 done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,34 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductCard } from './product-card/product-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ProductCard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angularp1');
+  products = [
+    {
+      title: 'Nike',
+      description: 'Dunk High Green Satin Sneakers',
+      price: 180,
+    },
+    {
+      title: 'Adidas',
+      description: 'Ultraboost 22 Shoes',
+      price: 150,
+    },
+    {
+      title: 'cumpus',
+      description: 'Ultraboost 22 Shoes',
+      price: 150,
+    },
+    {
+      title: 'puma',
+      description: 'Ultraboost 22 Shoes',
+      price: 150,
+    },
+  ];
 }
---
+++ src/app/product-card/product-card.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-product-card',
+  imports: [],
+  templateUrl: './product-card.html',
+  styleUrl: './product-card.css',
+})
+export class ProductCard {
+  product = input<any>();
+ 
+}
